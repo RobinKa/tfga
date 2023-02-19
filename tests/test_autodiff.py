1cd85f1350aed2fcdbe77e3194637379cb246ff2
Robin Kahlow <tora@warlock.ai>
2023-02-19 17:50:55 +0000

    add make rotor test

---
+++ tests/test_autodiff.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+from tfga import GeometricAlgebra
+from tfga.blades import BladeKind
+
+algebra = GeometricAlgebra([1, 1, 1])
+
+
+def get_rotor_loss(values):
+    rotor = algebra.from_tensor_with_kind(values, BladeKind.BIVECTOR)
+    return tf.reduce_sum(
+        tf.math.square(algebra.geom_prod(rotor, algebra.reversion(rotor) - 1))
+    )
+
+
+def test_make_rotor():
+    rotor_values = tf.Variable([1, 2, 3], dtype=tf.float32)
+
+    optimizer = tf.optimizers.Adam(1)
+
+    def train_step():
+        with tf.GradientTape() as tape:
+            tape.watch(rotor_values)
+            loss = get_rotor_loss(rotor_values)
+
+        grads = tape.gradient(loss, rotor_values)
+        optimizer.apply_gradients(zip([grads], [rotor_values]))
+
+    for _ in range(100):
+        train_step()
+
+    final_loss = get_rotor_loss(rotor_values)
+    assert final_loss < 0.1
